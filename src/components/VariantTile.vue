8dfa5710776fbe772fd649587064f32428463d39
Hosein Khansari <hrk.x11@gmail.com>
2018-04-25 18:13:24 +0430

    refactor VariantTile

--- src/components/VariantTile.vue
+++ src/components/VariantTile.vue
@@ -1,47 +1,28 @@
 <template>
-	<v-list-tile v-ripple avatar ref='root'>
-		<v-list-tile-avatar>
-			<v-avatar :style='{"background-color": this.hex}' style='flex-shrink: 0' :size='32'></v-avatar>
-		</v-list-tile-avatar>
-		<v-list-tile-content>
-			<v-list-tile-title>{{name}}</v-list-tile-title>
-			<v-list-tile-sub-title>{{this.hex}}</v-list-tile-sub-title>
-		</v-list-tile-content>
-	</v-list-tile>
+    <v-list-tile v-ripple avatar ref='root'>
+        <v-list-tile-avatar>
+            <v-avatar :style='{"background-color": this.hex}' style='flex-shrink: 0' :size='32'></v-avatar>
+        </v-list-tile-avatar>
+        <v-list-tile-content>
+            <v-list-tile-title class="var-title">{{this.variantName}}</v-list-tile-title>
+            <v-list-tile-sub-title class="var-hex">{{this.hex}}</v-list-tile-sub-title>
+        </v-list-tile-content>
+    </v-list-tile>
 </template>
 
 <script>
 export default {
-	name: 'variant-tile',
-	props: {
-		variantName: String,
-		hex: String
-	},
-	computed: {
-		name(){
-			const v = this.variantName;
-			return v.charAt(0).toUpperCase() + v.substring(1, v.length) + ' color'
-		}
-	},
-	mounted(){
-		let {$el} = this.$refs.root;
-
-		const trigger = () => this.$emit('active')
-
-		$el.addEventListener('click', trigger)
-
-		let timer = null;
-
-		$el.addEventListener('mousedown', () => {
-			timer = setTimeout(trigger, 3000)
-		})
-		$el.addEventListener('mouseup', () => {
-			clearInterval(timer)
-		})
-	}
+    name: 'variant-tile',
+    props: {
+        variantName: String,
+        hex: String
+    }
 }
 </script>
 
-<style scoped>
-
+<style lang='stylus' scoped>
+    .var-title
+        text-transform: capitalize
+    .var-hex
+        text-transform: uppercase
 </style>
