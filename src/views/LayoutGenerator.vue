79579b69cad86325b939f58dfa669baae84f5c1a
Hosein Khansari <hrk.x11@gmail.com>
2018-05-11 13:54:20 +0430

    add cache for layout tool

--- src/views/LayoutGenerator.vue
+++ src/views/LayoutGenerator.vue
@@ -1,6 +1,6 @@
 <template>
 	<div ref='root'>
-		<v-toolbar app fixed :clipped-left="drawer.clipped" flat :dense='toolbar.dense'>
+		<v-toolbar app fixed :clipped-left="l.drawer.clipped" :dense='l.toolbar.dense'>
 			<v-toolbar-side-icon @click.native="showDrawer = !showDrawer"></v-toolbar-side-icon>
 			<v-toolbar-title>Layout</v-toolbar-title>
 			<v-spacer></v-spacer>
@@ -8,45 +8,43 @@
 				Back to home
 			</v-btn>
 		</v-toolbar>
-		<v-navigation-drawer app :temporary='!drawer.permanent' v-model='showDrawer' :clipped="drawer.clipped" :permanent="drawer.permanent" :floating="drawer.floating" ref='drawer'>
-		  
-		</v-navigation-drawer>
+		<v-navigation-drawer app :temporary='!l.drawer.permanent' v-model='showDrawer' :clipped="l.drawer.clipped" :permanent="l.drawer.permanent" :floating="l.drawer.floating"></v-navigation-drawer>
 		<v-content>
-			<v-card class="ma-3 py-2">
+			<v-card class="ma-3">
 				<v-list subheader two-line>
 					<v-subheader>Navigation Drawer</v-subheader>
 					<v-list-tile>
 						<v-list-tile-action>
-							<v-checkbox v-model="drawer.permanent"></v-checkbox>
+							<v-checkbox v-model="l.drawer.permanent"></v-checkbox>
 						</v-list-tile-action>
 						<v-list-tile-content>
 							<v-list-tile-title>Always visible (permanent)</v-list-tile-title>
-						  	<v-list-tile-sub-title>Make drawer visibile on any screen</v-list-tile-sub-title>
+							<v-list-tile-sub-title>Make drawer visibile on any screen</v-list-tile-sub-title>
 						</v-list-tile-content>
 					</v-list-tile>
-					<v-list-tile :disabled='!drawer.permanent'>
+					<v-list-tile :disabled='!l.drawer.permanent'>
 						<v-list-tile-action>
-							<v-checkbox v-model="drawer.clipped"></v-checkbox>
+							<v-checkbox v-model="l.drawer.clipped"></v-checkbox>
 						</v-list-tile-action>
 						<v-list-tile-content>
 							<v-list-tile-title>Clipped</v-list-tile-title>
-						  	<v-list-tile-sub-title>Make toolbar come over drawer (has no effect on temporary drawers)</v-list-tile-sub-title>
+							<v-list-tile-sub-title>Make toolbar come over drawer (has no effect on temporary drawers)</v-list-tile-sub-title>
 						</v-list-tile-content>
 					</v-list-tile>
 					<v-list-tile>
 						<v-list-tile-action>
-							<v-checkbox v-model="drawer.floating"></v-checkbox>
+							<v-checkbox v-model="l.drawer.floating"></v-checkbox>
 						</v-list-tile-action>
 						<v-list-tile-content>
 							<v-list-tile-title>Floating drawer</v-list-tile-title>
-						  	<v-list-tile-sub-title>Hide drawer border</v-list-tile-sub-title>
+							<v-list-tile-sub-title>Hide drawer border</v-list-tile-sub-title>
 						</v-list-tile-content>
 					</v-list-tile>
 					<v-divider></v-divider>
 					<v-subheader>Toolbar</v-subheader>
 					<v-list-tile>
 						<v-list-tile-action>
-							<v-checkbox v-model="toolbar.dense"></v-checkbox>
+							<v-checkbox v-model="l.toolbar.dense"></v-checkbox>
 						</v-list-tile-action>
 						<v-list-tile-content>
 							<v-list-tile-title>Dense toolbar</v-list-tile-title>
@@ -55,9 +53,9 @@
 					</v-list-tile>
 					<v-divider></v-divider>
 					<v-subheader>Footer</v-subheader>
-					<v-list-tile :disabled="!drawer.permanent">
+					<v-list-tile :disabled="!l.drawer.permanent">
 						<v-list-tile-action>
-							<v-checkbox v-model="footer.inset"></v-checkbox>
+							<v-checkbox v-model="l.footer.inset"></v-checkbox>
 						</v-list-tile-action>
 						<v-list-tile-content>
 							<v-list-tile-title>Inset footer</v-list-tile-title>
@@ -67,7 +65,7 @@
 				</v-list>
 			</v-card>
 		</v-content>
-		<v-footer class="caption" app :inset='footer.inset' ref='footer'>
+		<v-footer class="caption" app :inset='l.footer.inset'>
 			<div class="px-2">
 				Copyright &copy; &mdash; Your company
 			</div>
@@ -81,20 +79,30 @@ export default {
 	name: 'layout-generator',
 	data() {
 		return {
-			footer: {
-				inset: true,
-				appendToDrawer: false
-			},
-			drawer: {
-				clipped: false,
-				floating: false,
-				permanent: false
-			},
-			toolbar: {
-				dense: false,
+			l: {
+				footer: {
+					inset: true,
+					appendToDrawer: false
+				},
+				drawer: {
+					clipped: false,
+					floating: false,
+					permanent: false
+				},
+				toolbar: {
+					dense: false,
+				}
 			},
 			showDrawer: false
 		}
+	},
+	created(){
+		this.l = JSON.parse(localStorage.getItem('layout--obj')) || this.l
+		this.$watch('l', (e) => {
+			localStorage.setItem('layout--obj', JSON.stringify(e))
+		}, {
+			deep: true
+		})
 	}
 }
 </script>
